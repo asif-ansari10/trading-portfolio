d3d90e5afc77bb922c1117a0b14b9688d4d9e106
Asif Ansari <asifnasimansari10@gmail.com>
2025-12-01 00:30:58 +0530

    Updated files

--- app/api/trades/get-holdings/route.js
+++ app/api/trades/get-holdings/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import connectToDB from "@/lib/db";
+import {connectToDB} from "@/lib/db";
 import Holding from "@/models/Holding";
 
 export async function GET() {
